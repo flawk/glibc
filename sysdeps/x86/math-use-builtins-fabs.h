6bc8d648dc576efde51e34e1190f63e61b1a2885
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2022-04-01 11:04:53 -0300

    math: Handle fabsf128 for clang

--- sysdeps/x86/math-use-builtins-fabs.h
+++ sysdeps/x86/math-use-builtins-fabs.h
@@ -1,4 +1,8 @@
 /* Generic implementation already assume builtin support for
    float and double.  */
 #define USE_FABSL_BUILTIN 1
-#define USE_FABSF128_BUILTIN 1
+#ifdef __clang__
+# define USE_FABSF128_BUILTIN 0
+#else
+# define USE_FABSF128_BUILTIN 1
+#endif
